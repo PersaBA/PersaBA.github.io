febc715b1625d01ed0da37eeb5d5d24da07c0e3b
PersaBA <89327031+PersaBA@users.noreply.github.com>
2022-05-31 12:00:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 var ID = "ig";
 
 var IG = document.getElementById('aIg');
-IG.onclick = function () { 
+IIG.onclick = function () { 
     var color = document.getElementById('ig').style.backgroundColor = 'white';
-    var color = document.getElementById('li-email').style.backgroundColor = 'gray';
-    var color = document.getElementById('In').style.backgroundColor = 'gray';
-    var color = document.getElementById('tw').style.backgroundColor = 'gray';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
  };
 
  var EMAIL = document.getElementById('aEmail');
  EMAIL.onclick = function () { 
-    var color = document.getElementById('ig').style.backgroundColor = 'gray';
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
     var color = document.getElementById('li-email').style.backgroundColor = 'white';
-    var color = document.getElementById('In').style.backgroundColor = 'gray';
-    var color = document.getElementById('tw').style.backgroundColor = 'gray';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
   };
 
   var IN = document.getElementById('aIn');
   IN.onclick = function () { 
-    var color = document.getElementById('ig').style.backgroundColor = 'gray';
-    var color = document.getElementById('li-email').style.backgroundColor = 'gray';
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
     var color = document.getElementById('In').style.backgroundColor = 'white';
-    var color = document.getElementById('tw').style.backgroundColor = 'gray';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
  };
 
  var TW = document.getElementById('aTw');
  TW.onclick = function () { 
-    var color = document.getElementById('ig').style.backgroundColor = 'gray';
-    var color = document.getElementById('li-email').style.backgroundColor = 'gray';
-    var color = document.getElementById('In').style.backgroundColor = 'gray';
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
     var color = document.getElementById('tw').style.backgroundColor = 'white';
  };
 
