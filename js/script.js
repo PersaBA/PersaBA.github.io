ac004ddc8fd7d4ead88fd8a1f9241bb241a928b0
PersaBA <89327031+PersaBA@users.noreply.github.com>
2022-05-31 15:18:41 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+var ID = "ig";
+
+var IG = document.getElementById('aIg');
+IG.onclick = function () { 
+    var color = document.getElementById('ig').style.backgroundColor = '#0e0e0e';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
+ };
+
+ var EMAIL = document.getElementById('aEmail');
+ EMAIL.onclick = function () { 
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
+    var color = document.getElementById('li-email').style.backgroundColor = '#0e0e0e';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
+  };
+
+  var IN = document.getElementById('aIn');
+  IN.onclick = function () { 
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
+    var color = document.getElementById('In').style.backgroundColor = '#0e0e0e';
+    var color = document.getElementById('tw').style.backgroundColor = '#00000000';
+ };
+
+ var TW = document.getElementById('aTw');
+ TW.onclick = function () { 
+    var color = document.getElementById('ig').style.backgroundColor = '#00000000';
+    var color = document.getElementById('li-email').style.backgroundColor = '#00000000';
+    var color = document.getElementById('In').style.backgroundColor = '#00000000';
+    var color = document.getElementById('tw').style.backgroundColor = '#0e0e0e';
+ };
